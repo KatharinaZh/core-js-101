08095fc72f333b8e132444fbdab34f42c5500447
Zhdanovich Katsiaryna <zhdanovichkat@gmail.com>
2019-12-19 11:13:14 +0300

    feat: complete tasks from 05(all)

--- src/05-regex-tasks.js
+++ src/05-regex-tasks.js
@@ -32,7 +32,8 @@
  * @return {RegExp}
  */
 function getRegexForGuid() {
-  throw new Error('Not implemented');
+  const reg = /{[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}}/;
+  return reg;
 }
 
 
@@ -54,7 +55,8 @@ function getRegexForGuid() {
  *
  */
 function getRegexForPitSpot() {
-  throw new Error('Not implemented');
+  const reg = /p.t|sp|sl/;
+  return reg;
 }
 
 
@@ -78,8 +80,8 @@ function getRegexForPitSpot() {
  *   'PASSW0RD'.match(validator)  => false
  *   'Pa55'.match(validator) => false
  */
-function getPasswordValidator(/* minLength */) {
-  throw new Error('Not implemented');
+function getPasswordValidator(minLength) {
+  return new RegExp(`^((?=\\w*[a-z])(?=\\w*[A-Z])(?=\\w*[0-9])[a-zA-Z0-9]{${minLength},})$`);
 }
 
 
